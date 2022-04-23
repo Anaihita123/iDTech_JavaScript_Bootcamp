8cbc2ed24f78cce05f4eaf463aa8989c8aa61e3d
Anaihita123 <103839944+Anaihita123@users.noreply.github.com>
2022-04-23 21:40:39 +0530

    Add files via upload

---
+++ Fortune Teller Activity/Session 1 - Fortune Teller.js
@@ -0,0 +1,12 @@
+let fortune = 1;
+if (fortune >= 0 && fortune <= 3) {
+    console.log("you have a low fortune.")
+}
+let fortune = 5;
+else if(fortune > 3 && fortune <= 7) {
+    console.log("you have an average fortune")
+}
+let fortune = 34;
+else {
+    console.log("the fortune can't be read correctly")
+}
